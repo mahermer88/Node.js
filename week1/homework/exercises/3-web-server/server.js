3e704f0436fc90c824e03785d6524e83c4277faf
Maher <ma.meer@hotmail.com>
2021-04-12 00:36:02 +0200

    homework-week1

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -1,14 +1,26 @@
 /**
  * Exercise 3: Create an HTTP web server
  */
+const { readFileSync } = require(`fs`);
+const homePage = readFileSync("./index.html");
+const JavaScript = readFileSync("./index.js");
+const style = readFileSync("./style.css");
 
-var http = require('http');
+var http = require("http");
 
 //create a server
 let server = http.createServer(function (req, res) {
   // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end(homePage);
+  } else if (req.url === "/index.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.end(JavaScript);
+  } else if (req.url === "/style.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.end(style);
+  }
 });
 
 server.listen(3000); // The server starts to listen on port 3000
